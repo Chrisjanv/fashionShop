8d941563dd51989e91a1f9ae17d970524568c45b
Andre Visser <chrisjanv@gmail.com>
2023-04-16 10:47:02 +0200

    database created

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,4 +1,3 @@
-
 // Back to top button scrolling and functionality
 const topButton = document.getElementById("topButton");
 topButton.addEventListener("click", function () {
@@ -8,29 +7,21 @@ topButton.addEventListener("click", function () {
     });
 });
 
-// Retrieve clothing 'stock database'
+// Contact form
 
 // Shop
-
-
-
-
-
-
-
-
-
-
-
+// Retrieve clothing 'stock database'
+fetch('/src/json/clothing.json')
+    .then(response => response.json())
+    .then(data => {
+        // Do something with the JSON data
+        console.log(data); // Print the data to the console
+    })
+    .catch(error => {
+        console.error('Error fetching JSON data: ', error);
+});
 
 // gallery
-
-
-
-
-// Form tied to backend
-
-// display specials on home page from the shop page
 
 
 
