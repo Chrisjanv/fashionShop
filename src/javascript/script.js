35fe9d4f8b3a4dcbd4b6b974ef38c83f1074cad8
Andre Visser <chrisjanv@gmail.com>
2023-04-16 21:19:04 +0200

    simple fetch

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -10,23 +10,15 @@ topButton.addEventListener("click", function () {
 // Contact form
 
 // Shop
-let clothingArray =[];
-// Retrieve clothing 'stock database'
 fetch('/src/json/clothing.json')
     .then(response => response.json())
     .then(data => {
-        // Parse the JSON data into a JavaScript object array
-        clothingArray = data.clothing;
-        console.log(clothingArray); // Print the clothing array to the console
+        // Do something with the JSON data
+        console.log(data);
     })
     .catch(error => {
         console.error('Error fetching JSON data: ', error);
     });
 
-console.log(clothingArray[2]);
-// Wait for the fetch function to complete before using the clothingArray data
-setTimeout(() => {
-    console.log(clothingArray[2]);
-}, 1000); // Wait for 1 second to give time for the fetch function to complete
-
 // gallery
+
