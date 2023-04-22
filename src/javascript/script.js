7d29269e5120e459e531d2ff8fbba1830c19d6ba
Andre Visser <chrisjanv@gmail.com>
2023-04-22 18:13:51 +0200

    Testing bootstrap card injection through js

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -10,10 +10,10 @@ topButton.addEventListener("click", function () {
 // Contact form
 const form = document.getElementById('ContactForm');
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    // Anything needed for the form
-});
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault();
+//     // Anything needed for the form
+// });
 
 
 // Shop
@@ -21,14 +21,37 @@ fetch('/src/json/clothing.json')
     .then(response => response.json())
     .then(data => {
         // Do something with the JSON data
+        let itemsForSale = [];
+        itemsForSale = data;
         console.log(data);
     })
     .catch(error => {
         console.error('Error fetching JSON data: ', error);
 });
 
+// function listItems() {
+// for (let i = 0; i < itemsForSale.length; i++) {
 
+// }
+// }
 
+// Get a reference to the div with an ID of "shopPage"
+const shopPageDiv = document.getElementById("shopPage");
+
+// Define the HTML code you want to inject
+const html = `
+    <div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" class="btn">Go somewhere</a>
+      </div>
+    </div>
+`;
+
+// Inject the HTML code into the shopPageDiv using innerHTML
+shopPageDiv.innerHTML = html;
 
 // gallery
 
