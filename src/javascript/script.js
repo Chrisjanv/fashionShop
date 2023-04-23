6179ca2f24f37b50d27bca90d008ea7ff69cca55
Andre Visser <chrisjanv@gmail.com>
2023-04-23 09:01:12 +0200

    text/css

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -78,13 +78,6 @@ fetch('/src/json/clothing.json')
 });
 
 // Add to cart function
-class Cart {
-    constructor(product, price) {
-        this.product = name;
-        this.price = age;
-    }
-}
-
  function addToCart() {
     alert('Add to cart function');
 }
@@ -93,7 +86,7 @@ function clearCart() {}
 
 class Cart{
     constructor(product, price) {
-        this.product = name;
+        this.product = product;
         this.price = age;
     }
 }
